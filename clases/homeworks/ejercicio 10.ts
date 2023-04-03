ceab7249c6276ae93bb5ae20f4871757dd720a31
MaxMdr2022 <maxi_meder94@hotmail.com>
2023-04-03 13:56:39 -0300

    ejercicio 10

--- clases/homeworks/ejercicio 10.ts
+++ clases/homeworks/ejercicio 10.ts
@@ -10,3 +10,5 @@ interface Instructor {
   const noTypeVar = {name: "Franco", age: 26};
   
   infoInstructor(noTypeVar); // "Franco - 26 years old"
+
+  // infoInstructor({nombre: "maxi"}) // Argument of type '{ nombre: any; }' is not assignable to parameter of type 'Instructor'. Object literal may only specify known properties, and 'nombre' does not exist in type 'Instructor'.
