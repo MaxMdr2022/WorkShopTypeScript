d8c6ff0958feb0069da76f4621126fe850ed0445
MaxMdr2022 <maxi_meder94@hotmail.com>
2023-04-03 13:56:12 -0300

    e7 y 8

--- clases/homeworks/ejercicio 7.ts
+++ clases/homeworks/ejercicio 7.ts
@@ -1,4 +1,26 @@
-function suma(a: number, b: number): number {
+function suma2(a: number, b: number): number {
     return a + b
 }
 // Logra que esta funcion tambien concatene strings
+
+function suma3(a: number | string, b: number | string): any{
+
+
+    if(typeof a === "string" && typeof b === "string"){
+
+        return a.concat(b)
+    }
+
+    if(typeof a === "number" && typeof b === "number"){
+
+        return a + b
+    }
+
+
+};
+
+console.log(suma3("hola", " maxi")) // hola maxi
+
+console.log(suma3(23,23)) // 46
+
+console.log(suma3("hola", 23)) // undefined
